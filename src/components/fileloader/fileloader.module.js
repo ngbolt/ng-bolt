45d6412d760cf9000ab0411d78c35940cfa343ac
ats5364 <ats5364@arl.psu.edu>
2017-06-26 09:33:06 -0400

    Fixed documention's merge conflict

--- src/components/fileloader/fileloader.module.js
+++ src/components/fileloader/fileloader.module.js
@@ -48,23 +48,15 @@
    *
    * @restrict E
    *
+   * @param {string} data-label This attribute specifies the label for this component.
+   * @param {string} data-name This attribute indicates the name of this form element and will be used during form
+   * traversal by the ngBoltJS framework.
    * @param {boolean} [data-autofocus] Indicates whether or not this field should autofocus on page load.
    * @param {expression} [data-change] This attribute is used to bind an expression in the containing scope that
    * will be invoked any time the value of this component changes. Functionality is based on the Angular ngChange
    * directive.
    * @param {boolean} [data-disabled] Disables the field. Any value set in this attribute will cause the field to be
    * disabled.
-   * @param {string} data-label This attribute specifies the label for this component.
-   * @param {string} data-name This attribute indicates the name of this form element and will be used during form
-   * traversal by the ngBoltJS framework.
-<<<<<<< HEAD
-=======
-   * @param {expression} [data-model] This attribute is used to bind the value of this component to a property in the
-   * containing scope. Functionality is based on the Angular ngModel directive.
-   * @param {boolean} [data-disabled] Disables the field. Any value set in this attribute will cause the field to be
-   * disabled.
-   * @param {boolean} [data-autofocus] Indicates whether or not this field should autofocus on page load.
->>>>>>> ac3927ff77cff3053df716b87c3f2d660aa00c2d
    * @param {value} [data-required] Indicates whether or not this field is required.
    * @param {value} [data-tabindex] Specifies the tab order of an element
    * @param {expression} [data-validate] An expression that gets passed through to an instance of the bltValidate
